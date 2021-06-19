f2a1bcae2d723685da553e8090cae2977d2bd59e
Adam-Palacz <adam.palacz96@gmail.com>
2021-06-19 14:02:42 +0200

    add dynamic statisitcs

--- charity_project/charity_app/models.py
+++ charity_project/charity_app/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Institution(models.Model):
+    TYPE_CHOICES = (
+        (1, "fundacja"),
+        (2, "organizacja pozarządowa"),
+        (3, "zbiórka lokalna"),
+        (4, "domyślna fundacja"),
+    )
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    type = models.IntegerField(choices=TYPE_CHOICES, default=4)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=9)
+    city = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=10)
+    pick_up_date = models.DateField(null=True)
+    pick_up_time = models.TimeField(null=True)
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
--- charity_project/charity_app/views.py
+++ charity_project/charity_app/views.py
@@ -1,14 +1,48 @@
 from django.shortcuts import render
 from django.views import View
+from django.core.paginator import Paginator
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from .models import Category, Donation, Institution
+
 
 
 class LandingPageView(View):
     def get(self, request):
-        return render(request, "index.html")
+        quantity_counter = sum(
+            [Donation.objects.values_list()[i][1] for i in range(len(Donation.objects.values_list()))]
+        )
+        institution_counter = len(Institution.objects.all())
 
+        foundations = Institution.objects.filter(type=1)
+        organizations = [Institution.objects.filter(type=2)[i] for i in range(len(Institution.objects.filter(type=2)))]
+        fundraisings = [Institution.objects.filter(type=3)[i] for i in range(len(Institution.objects.filter(type=3)))]
+
+        paginator_foundation = Paginator(foundations, 5)
+        paginator_organization = Paginator(organizations, 5)
+        paginator_fundraising = Paginator(fundraisings, 5)
+        page_number = request.GET.get('page')
+        page_obj_foundation = paginator_foundation.get_page(page_number)
+        page_obj_organization = paginator_organization.get_page(page_number)
+        page_obj_fundraising = paginator_fundraising.get_page(page_number)
+
+        context = {
+            'quantity': quantity_counter, 'institution_counter': institution_counter,
+            'foundations': foundations, 'organizations': organizations,
+            'fundraisings': fundraisings, "page_obj_foundation": page_obj_foundation,
+            "page_obj_organization": page_obj_organization, "page_obj_fundraising": page_obj_fundraising,
+        }
+        return render(request, "index.html", context=context)
+
+    def post(self,request):
+        logout_button = request.POST.get('logout')
+        if logout_button:
+            logout(request)
+            return render(request, "index.html")
 
 class AddDonationView(View):
     def get(self, request):
+
         return render(request, "form.html")
 
 
@@ -16,7 +50,37 @@ class LoginView(View):
     def get(self, request):
         return render(request, "login.html")
 
+    def post(self,request):
+        username = request.POST.get("email")
+        password = request.POST.get("password")
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            name = user.first_name
+            return render(request, "index.html", context={'username':name})
+        else:
+            return render(request, "register.html")
+        # try:
+        #     User.objects.get(username=username, password=password)
+        #     return render(request, "index.html")
+        # except ObjectDoesNotExist:
+        #     return render(request, "register.html")
 
 class RegisterView(View):
     def get(self, request):
         return render(request, "register.html")
+
+    def post(self, request):
+        name = request.POST.get("name")
+        surname = request.POST.get("surname")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        password2 = request.POST.get("password2")
+        if password != password2:
+            return render(request, "register.html")
+        else:
+            user = User.objects.create_user(username=email, password=password, email=email)
+            user.first_name = name
+            user.last_name = surname
+            user.save()
+            return render(request, "login.html")
